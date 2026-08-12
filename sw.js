eb20f04f42011829bcf0c298011252f75161aaad
Lorenzogtp <lorenzozanoni64@gmail.com>
2026-08-12 10:04:17 +0200

    Aggiornamento Fenice Palcoscenico v2

--- sw.js
+++ sw.js
@@ -1,20 +1,9 @@
-const CACHE = 'fenice-palco-v1';
-const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return resp;
-    }).catch(() => caches.match('./index.html')))
-  );
+const CACHE='fenice-palco-v2-20260812';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;})
+ .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
 });
